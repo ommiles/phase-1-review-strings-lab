3b466781ee5b78da9f81e503bdba354db1fb7896
OM Miles <olivia.m.miles@gmail.com>
2021-04-05 11:10:48 -0700

    Tests complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = `Grace Hopper` 
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+// interesting!
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
